7b5be3da907513896bc346a4cb3d615c5b43ae77
Pechenyka <badashov.den@gmail.com>
2025-02-13 08:02:46 +0000

    include phpmyadmin && filament

--- sail_run.sh
+++
@@ -1,3 +0,0 @@
-./vendor/bin/sail up -d
-./vendor/bin/sail npm run build
-./vendor/bin/sail artisan serve
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,13 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
+import preset from './vendor/filament/support/tailwind.config.preset'
 
 /**defaultTheme @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './resources/**/*.tsx'
+        './resources/**/*.tsx',
+        './app/Filament/**/*.php',
+        './resources/views/**/*.blade.php',
+        './vendor/filament/**/*.blade.php',
     ],
     theme: {
         extend: {
@@ -13,4 +17,5 @@ export default {
         },
     },
     plugins: [],
+    presets: [preset],
 };
